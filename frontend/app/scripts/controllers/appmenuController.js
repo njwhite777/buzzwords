b39feec4a7166c6e9c6ede1275a9ebf8d40c117f
hanqingguo <18523978636@163.com>
2018-03-24 15:25:36 -0400

    add views

--- frontend/app/scripts/controllers/appmenuController.js
+++ frontend/app/scripts/controllers/appmenuController.js
@@ -2,13 +2,13 @@
 
 /**
  * @ngdoc function
- * @name frontendApp.controller:menuController
+ * @name frontendApp.controller:appmenuController
  * @description
  * # menuController
  * Controller of the left drawer of the application
  */
 angular.module('frontendApp')
-  .controller('menuController', [
+  .controller('appmenuController', [
     '$scope',
     'gameService',
     function ($scope,gameService) {
