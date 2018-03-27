d91e7751dcbd8360d3d9990f2579ffbef5a6ce00
Aron Kageza <agkageza@bsu.edu>
2018-03-27 18:47:59 -0400

    Updating models

--- backend/models/game.py
+++ backend/models/game.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import datetime
 from sqlalchemy import Column, Integer, String, Table, ForeignKey, DateTime
 from sqlalchemy.orm import sessionmaker, relationship
 from . import Base
@@ -15,14 +16,25 @@ class Game(Base):
     id          = Column(Integer, primary_key=True)
     name = Column(String)
     defaultRoundTime = Column(Integer)
-    createdTimeStamp = Column(DateTime)
+    createdTimeStamp = Column(DateTime, default=datetime.datetime.utcnow)
     initiator_id = Column(Integer, ForeignKey('player.id'), nullable=True)
     initiator = relationship("Player", foreign_keys = initiator_id, lazy = False, uselist=False)
     teams = relationship("Team", backref = "game", lazy = False)
     rounds = relationship("Round", backref = "game", lazy = False)
-    used_cards = relationship("Card", secondary=usedCards)
+    used_cards = relationship("Card", secondary=usedCards, lazy = False)
 
-    # TODO: what else will we need?
+    def __init__(self, name, initiator, defaultRoundTime ):
+        self.name = name
+        self.defaultRoundTime = defaultRoundTime
+        self.initiator = initiator
+        self.teams = []
+        self.used_cards = []
+
+    def add_used_card(self, card):
+        self.used_cards.append(card)
+
+    def set_teams(self, teams):
+        self.teams = teams
 
     def __repr__(self):
         return "<Game(id='{}', player_count='{}', cards_to_win='{}', number_of_teams='{}')>".format(
