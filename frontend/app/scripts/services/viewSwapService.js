c1e0607c8419a9b4ad3b01a9e3f18c48d28af174
hanqingguo <18523978636@163.com>
2018-03-29 16:02:16 -0400

    hanqing

--- frontend/app/scripts/services/viewSwapService.js
+++ frontend/app/scripts/services/viewSwapService.js
@@ -8,10 +8,39 @@
  */
 
 angular.module('frontendApp')
-  .service('viewSwapService',['socketService','$state','debug',function(socketService,$state,debug){
+  .service('viewSwapService',[
+    'socketService',
+    '$state',
+    'debug',
+    function(socketService,$state,debug){
     // This service listens to the created game event and adds a new game to the list when one is created.
     var socket = socketService.viewSocket;
+    var data = {};
 
+    socket.on('swap_view',function(message){
+      if(debug) console.log("viewswapservice:swap_view");
+      if(debug) console.log(message);
 
+      if(message['turn_view'] == 'moderatorturn'){
+        data = message['data'];
+        $state.go('moderatorturn');
+      }
+      if(message['turn_view'] == 'tellerrolldie'){
+        data = message['data'];
+        $state.go('tellerrolldie')
+      }
+      if(message['turn_view'] == 'tellerturn' ){
+        data = message['data'];
+        $state.go('tellerturn');
+      }
+      if(message['turn_view'] == 'gameplayerturn'){
+        data = message['data'];
+        $state.go('gameplayerturn');
+      }
+    });
+
+    return {
+      data : data
+    };
 
   }]);
