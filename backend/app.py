4a0ba4772c016c7ca21f0d194d0ad594f6af8922
Nathan White <njwhite777@gmail.com>
2018-04-20 11:43:36 -0400

    Fixed some issues that were keeping things from running.

--- backend/app.py
+++ backend/app.py
@@ -7,34 +7,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base as AppModelBase,CardModel
 from db import create_db
+import quizlet
 import argparse
 import sys
-
-app = Flask(__name__)
-app.config.from_object('settings')
-app.config.from_envvar('CS690_SETTINGS',silent=True)
-debug=app.config['DEBUG']
-socketio = SocketIO(app)
-#
-# TODO: more db setup based on the env that gets passed in.
-engine=None
-if(app.config['ENVIRONMENT'] == 'dev'):
-    print("RUNNING IN DEV")
-    engine = create_engine(app.config['DB_URI'])
-    if(app.config['REBUILDDB']):
-        create_db(engine)
-        AppModelBase.metadata.create_all(engine)
-elif(app.config['ENVIRONMENT'] == 'prod'):
-    engine = create_engine(app.config['DB_URI'].format(app.config['DBNAME']))
-else:
-    pass
-    # engine = create_engine(app.config['DB_URI'].format(app.config['DBNAME']))
-    # Test
-
-Session = sessionmaker(bind=engine)
-session = Session()
-socketIOClients = dict()
-turnTimers = dict()
+import json
 
 def getQuizletCards(login='Nathan_White34',client_id='SN77uEA94G',endpoint='258934949'):
     # Getting the cards from quizlet.
@@ -47,7 +23,35 @@ def getQuizletCards(login='Nathan_White34',client_id='SN77uEA94G',endpoint='2589
         definitions = term['definition'].split('\n')
         buzzword = term['term'].strip()
         buzzword.split()
-        card = CardModel(buzzword=buzzword,forbidden_words=json.dumps(definitions[:-1]),source=definitions[-1],source_page=' '.join(definitions[-1].split()[-2:]),is_phrase=(len(buzzword.split())>1),quizleEndpoint=endpoint)
+        card = CardModel(buzzword=buzzword,forbidden_words=json.dumps(definitions[:-1]),source=definitions[-1],source_page=' '.join(definitions[-1].split()[-2:]),is_phrase=(len(buzzword.split())>1),quizletEndpoint=endpoint)
         session.add(card)
     session.commit()
     session.close()
+
+
+app = Flask(__name__)
+app.config.from_object('settings')
+app.config.from_envvar('CS690_SETTINGS',silent=True)
+debug=app.config['DEBUG']
+socketio = SocketIO(app)
+#
+# TODO: more db setup based on the env that gets passed in.
+engine=None
+Session=None
+socketIOClients = dict()
+turnTimers = dict()
+
+if(app.config['ENVIRONMENT'] == 'dev'):
+    print("RUNNING IN DEV")
+    engine = create_engine(app.config['DB_URI'])
+    if(app.config['REBUILDDB']):
+        create_db(engine)
+        AppModelBase.metadata.create_all(engine)
+        Session = sessionmaker(bind=engine)
+        getQuizletCards()
+elif(app.config['ENVIRONMENT'] == 'prod'):
+    engine = create_engine(app.config['DB_URI'].format(app.config['DBNAME']))
+else:
+    pass
+    # engine = create_engine(app.config['DB_URI'].format(app.config['DBNAME']))
+    # Test
